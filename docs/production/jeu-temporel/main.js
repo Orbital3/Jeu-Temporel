55cbb868c92810b64420a2bd3fb190b006d2627c
PhilTrudel <61752595+PhilTrudel@users.noreply.github.com>
2022-02-01 11:32:23 -0500

    Js questions

--- docs/production/jeu-temporel/main.js
+++ docs/production/jeu-temporel/main.js
@@ -1,1 +1,67 @@
 import './style.scss'
+
+
+const myQuestions = [
+  {
+    question: "À quel point êtes vous préoccupez par l'environnement?",
+    answers: {
+      a: "Très peu",
+      b: "Pas beaucoup",
+      c: "Moyennement",
+      d: "Vraiment trop"
+    },
+    correctAnswer: "d"
+  },
+  {
+    question: "Quelle a été, selon vous, l'année la plus chaude de l'histoire?",
+    answers: {
+      a: "1946",
+      b: "1994",
+      c: "2005",
+      d: "2018"
+    },
+    correctAnswer: "d"
+  },
+  {
+    question: "La température moyenne a augmenté de combien depuis l'ère industrielle?",
+    answers: {
+      a: "1 degré",
+      b: "5 degré",
+      c: "0.24 degré",
+      d: "N'a pas augmenté"
+    },
+    correctAnswer: "a"
+  },
+  {
+    question: "Dans combien d'année la guerre du pétrole devrait commencer?",
+    answers: {
+      a: "10 ans",
+      b: "50 ans",
+      c: "75 ans",
+      d: "100 ans"
+    },
+    correctAnswer: "b"
+  },
+  {
+    question: "Entre 1990 et 2016, quelle est la part des catastrophes liées aux changements climatiques dans la totalité des catastrophes naturelles dans le monde?",
+    answers: {
+      a: "20%",
+      b: "50%",
+      c: "70%",
+      d: "100%"
+    },
+    correctAnswer: "c"
+  },
+  {
+    question: "Selon les Québécois, quelle est la raison principale qui les pousse à gaspiller des aliments?",
+    answers: {
+      a: "Le respect des dates de péremption",
+      b: "Le trop grand format de certains aliments vendus en épicerie",
+      c: "Ils manquent de temps pour cuisiner",
+      d: "Ils manquent de connaissances pour mieux conserver les aliments"
+    },
+    correctAnswer: "a"
+  },
+  
+
+];
