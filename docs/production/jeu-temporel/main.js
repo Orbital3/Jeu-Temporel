2103cd227efe8af7a03620972b1645f1d3d1a66e
PhilTrudel <61752595+PhilTrudel@users.noreply.github.com>
2022-02-01 12:51:21 -0500

    added script for Timer

--- docs/production/jeu-temporel/main.js
+++ docs/production/jeu-temporel/main.js
@@ -1,6 +1,24 @@
 import './style.scss'
 
+//Éléments pour le countdown timer
+const startingMinutes = 15;
+let time = startingMinutes * 60;
 
+const countdownEl = document.getElementById('countdown');
+
+setInterval(updateCountdown, 1000);
+
+function updateCountdown(){
+  const minutes = Math.floor(time / 60);
+  let seconds = time % 60;
+
+  seconds = seconds < 10 ? '0' + seconds : seconds;
+
+  countdownEl.innerHTML = `${minutes}: ${seconds}`;
+  time--;
+}
+
+// Questions pour le questionnaire
 const myQuestions = [
   {
     question: "À quel point êtes vous préoccupez par l'environnement?",
