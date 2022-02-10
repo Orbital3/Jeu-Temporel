438016f9198aba37111c8b3450045ebce3b3d2d5
PhilTrudel <61752595+PhilTrudel@users.noreply.github.com>
2022-02-10 15:03:35 -0500

    seems to work

--- docs/production/jeu-temporel/main.js
+++ docs/production/jeu-temporel/main.js
@@ -161,65 +161,67 @@ let questions = [
 const SCORE_POINTS = 100
 const MAX_QUESTIONS = 15
 
-startGame = () => {
-    questionCounter = 0
-    score = 0
-    availableQuestions = [...questions]
-    getNewQuestion()
+
+let startGame = () => {
+  questionCounter = 0
+  score = 0
+  availableQuestions = [...questions]
+  getNewQuestion()
 }
 
-getNewQuestion = () => {
-    if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
-        localStorage.setItem('mostRecentScore', score)
+let getNewQuestion = () => {
+  if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS ) {
+    localStorage.setItem('mostRecentScore', score)
 
-        return window.location.assign('/end.html')
-    }
+    return window.location.assign('/end.html')
+  }
 
-    questionCounter++
-    progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`
-    progressBarFull.style.width = `${(questionCounter/MAX_QUESTIONS) * 100}%`
-    
-    const questionsIndex = Math.floor(Math.random() * availableQuestions.length)
-    currentQuestion = availableQuestions[questionsIndex]
-    question.innerText = currentQuestion.question
+  questionCounter++
+  progressText.innerText = `Question ${questionCounter}`
+  
 
-    choices.forEach(choice => {
-        const number = choice.dataset['number']
-        choice.innerText = currentQuestion['choice' + number]
-    })
+  const questionsIndex = Math.floor(Math.random() * availableQuestions.length)
+  currentQuestion = availableQuestions[questionsIndex]
+  question.innerText = currentQuestion.question
 
-    availableQuestions.splice(questionsIndex, 1)
+  choices.forEach(choice => {
+    const number = choice.dataset['number']
+    choice.innerText = currentQuestion['choice' + number]
 
-    acceptingAnswers = true
+  })
+
+  availableQuestions.splice(questionsIndex, 1)
+
+  acceptingAnswers = true
 }
 
 choices.forEach(choice => {
-    choice.addEventListener('click', e => {
-        if(!acceptingAnswers) return
+  choice.addEventListener('click', e => {
+    if(!acceptingAnswers) return
 
-        acceptingAnswers = false
-        const selectedChoice = e.target
-        const selectedAnswer = selectedChoice.dataset['number']
+    acceptingAnswers = false
+    const selectedChoice = e.target
+    const selectedAnswer = selectedChoice.dataset['number']
 
-        let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect'
+    let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect'
 
-        if(classToApply === 'correct') {
-            incrementScore(SCORE_POINTS)
-        }
+    if (classToApply === 'correct') {
+      incrementScore(SCORE_POINTS)
+    }
 
-        selectedChoice.parentElement.classList.add(classToApply)
+    selectedChoice.parentElement.classList.add(classToApply)
+    
+    setTimeout(() => {
+      selectedChoice.parentElement.classList.remove(classToApply)
+      getNewQuestion()
 
-        setTimeout(() => {
-            selectedChoice.parentElement.classList.remove(classToApply)
-            getNewQuestion()
-
-        }, 1000)
-    })
+    }, 1000)
+  })
 })
 
-incrementScore = num => {
-    score +=num
-    scoreText.innerText = score
+let incrementScore = num => {
+  score +=num
+  scoreText.innerText = score
 }
 
 startGame()
